15610c4306ce60a98fb6588e181036a0fe7d9aea
AsmaaBaiche <asmaa.uvsq@gmail.com>
2026-09-16 11:00:46 +0100

    Question 3 — Constantes ZERO et UN

--- Fraction.java
+++ Fraction.java
@@ -2,6 +2,23 @@ public class Fraction {
     private int numerateur;
     private int denominateur;
 
+public Fraction(int numerateur, int denominateur) {
+    this.numerateur = numerateur;
+    this.denominateur = denominateur;
+}
+
+public Fraction(int numerateur) {
+    this(numerateur, 1);
+}
+
+public Fraction() {
+    this(0, 1);
+}
+
+ // Q3 : constantes
+    public static final Fraction ZERO = new Fraction(0, 1);
+    public static final Fraction UN = new Fraction(1, 1)
+    
     @Override
     public String toString() {
         return "Je suis une fraction.";
--- Main.java
+++ Main.java
@@ -2,5 +2,11 @@ public class Main {
 public static void main(String[] args) {
         Fraction f = new Fraction();
         System.out.println(f);
+        Fraction f1 = new Fraction(3, 4);
+Fraction f2 = new Fraction(5);
+Fraction f3 = new Fraction();
+assert f1.toString().equals("3/4");
+assert f2.toString().equals("5/1");
+assert f3.toString().equals("0/1");
     }
 }
